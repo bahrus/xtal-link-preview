bc9b27cef218d2c75bc6461f87476dbc482dc34a
bahrus <anderson.bruce.b@gmail.com>
2017-10-30 20:04:22 -0400

    add preview check

--- xtal-link-preview.js
+++ xtal-link-preview.js
@@ -13,6 +13,7 @@
     class XtalLinkPreview extends HTMLElement {
         constructor() {
             super();
+            this._preview = false;
             this.attachShadow({ mode: 'open' });
             this.style.display = "block";
         }
@@ -24,16 +25,35 @@
         get href() {
             return this._href;
         }
+        set preview(val) {
+            if (val) {
+                this.setAttribute('preview', '');
+            }
+            else {
+                this.removeAttribute('preview');
+            }
+        }
+        get preview() {
+            return this._preview;
+        }
         static get observedAttributes() {
             return [
                 /** @type {string} Url to preview
                  *
                 */
                 'href',
+                /** @type {string} Must be true to preview the url specified by href
+                 *
+                */
+                'preview'
             ];
         }
         loadHref() {
             //const _this = this;
+            if (!this._preview)
+                return;
+            if (!this._href)
+                return;
             fetch('https://cors-anywhere.herokuapp.com/http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=' + this._href + '&image_no=1&css=true')
                 .then((response) => {
                 response.text().then(respText => {
@@ -46,6 +66,10 @@
                 case 'href':
                     this._href = newValue;
                     // if(this._once) this.loadHref();
+                    this.loadHref();
+                    break;
+                case 'preview':
+                    this._preview = newValue !== null;
                     this.loadHref();
                     break;
             }
--- xtal-link-preview.ts
+++ xtal-link-preview.ts
@@ -16,6 +16,7 @@
             this.style.display="block";
         }
         _href: string;
+        _preview = false;
         set href(val: string){
             this.setAttribute('href', val);
             //this._href = val;
@@ -24,16 +25,32 @@
         get href(){
             return this._href;
         }
+        set preview(val: boolean){
+            if(val){
+                this.setAttribute('preview', '');
+            }else{
+                this.removeAttribute('preview');
+            }
+        }
+        get preview(){
+            return this._preview;
+        }
         static get observedAttributes() {
             return [
                  /** @type {string} Url to preview
                   * 
                  */
                 'href',
+                /** @type {string} Must be true to preview the url specified by href
+                 * 
+                */
+                'preview'
             ];
         }
         loadHref(){
             //const _this = this;
+            if(!this._preview) return;
+            if(!this._href) return;
             fetch('https://cors-anywhere.herokuapp.com/http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=' + this._href + '&image_no=1&css=true')
             .then((response) =>{
                 response.text().then(respText =>{
@@ -48,6 +65,11 @@
                     // if(this._once) this.loadHref();
                     this.loadHref();
                     break;
+                case 'preview':
+                    this._preview = newValue !== null;
+                    this.loadHref();
+                    break;
+
             }
         }
     }
