28fc2c3ffdd67dd3736b3f83a4d3de9b8ef2df41
bahrus <anderson.bruce.b@gmail.com>
2018-03-25 15:01:38 -0400

    issue 1

--- xtal-link-preview.js
+++ xtal-link-preview.js
@@ -1,101 +1,127 @@
 (function () {
     if (customElements.get('xtal-link-preview'))
         return;
-    /**
-    * `xtal-link-preview`
-    * Provide preview of URL.
-    *
-    *
-    * @customElement
-    * @polymer
-    * @demo demo/index.html
-    */
-    class XtalLinkPreview extends HTMLElement {
-        constructor() {
-            super();
-            this._preview = false;
-            const template = document.createElement('template');
-            template.innerHTML = `
-              <style>
-                :host {
-                  display: block;
-                }
-              </style>
-              <slot></slot>
-            `;
-            this.attachShadow({ mode: 'open' });
-            this.shadowRoot.appendChild(template.content.cloneNode(true));
-            this.style.display = "block";
-        }
-        set href(val) {
-            this.setAttribute('href', val);
-            //this._href = val;
-            //this.loadHref();
-        }
-        get href() {
-            return this._href;
-        }
-        set preview(val) {
-            if (val) {
-                this.setAttribute('preview', '');
-            }
-            else {
-                this.removeAttribute('preview');
-            }
-        }
-        get preview() {
-            return this._preview;
-        }
-        static get observedAttributes() {
-            return [
-                /** @type {string} Url to preview
-                 *
-                */
-                'href',
-                /** @type {string} Must be true to preview the url specified by href
-                 *
-                */
-                'preview'
-            ];
-        }
-        loadHref() {
-            //const _this = this;
-            if (!this._preview)
-                return;
-            if (!this._href)
-                return;
-            fetch('https://cors-anywhere.herokuapp.com/http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=' + this._href + '&image_no=1&css=true')
-                .then((response) => {
-                response.text().then(respText => {
-                    let massagedText = respText;
-                    //console.log(massagedText);
-                    const replacements = ['html', 'head', 'body'];
-                    replacements.forEach(s => {
-                        massagedText = massagedText.replace('<' + s + '>', '<div>').replace('</' + s + '>', '</div>');
-                    });
-                    massagedText = massagedText.replace('<a href="', '<a target="_blank" href="');
-                    //console.log(massagedText);
-                    massagedText = massagedText.replace('<div id="toolbar" class="clearfix"><button id="changeimg">></button></div>', '');
-                    //const massagedText = respText.replace('<html>', '<div>')
-                    this.shadowRoot.innerHTML = massagedText;
-                });
-            });
-        }
-        attributeChangedCallback(name, oldValue, newValue) {
-            switch (name) {
-                case 'href':
-                    this._href = newValue;
-                    // if(this._once) this.loadHref();
-                    this.loadHref();
-                    break;
-                case 'preview':
-                    this._preview = newValue !== null;
-                    this.loadHref();
-                    break;
-            }
-        }
+    const cs = document.currentScript;
+    let customStyle = '';
+    if (cs && cs.dataset.css_path) {
     }
-    customElements.define('xtal-link-preview', XtalLinkPreview);
+    else {
+        initXtalLinkPreview();
+    }
+    function initXtalLinkPreview() {
+        /**
+        * `xtal-link-preview`
+        * Provide preview of URL.
+        *
+        *
+        * @customElement
+        * @polymer
+        * @demo demo/index.html
+        */
+        class XtalLinkPreview extends HTMLElement {
+            constructor() {
+                super();
+                this._serviceUrl = 'https://cors-anywhere.herokuapp.com/http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=';
+                this._preview = false;
+                const template = document.createElement('template');
+                template.innerHTML = `
+          <style>
+            :host {
+              display: block;
+            }
+          </style>
+          <slot></slot>
+        `;
+                this.attachShadow({ mode: 'open' });
+                this.shadowRoot.appendChild(template.content.cloneNode(true));
+                this.style.display = "block";
+            }
+            get serviceUrl() {
+                return this._serviceUrl;
+            }
+            set serviceUrl(val) {
+                this.setAttribute('service-url', val);
+            }
+            set href(val) {
+                this.setAttribute('href', val);
+                //this._href = val;
+                //this.loadHref();
+            }
+            get href() {
+                return this._href;
+            }
+            set preview(val) {
+                if (val) {
+                    this.setAttribute('preview', '');
+                }
+                else {
+                    this.removeAttribute('preview');
+                }
+            }
+            get preview() {
+                return this._preview;
+            }
+            static get observedAttributes() {
+                return [
+                    /** @type {string} Url to preview
+                     *
+                    */
+                    'href',
+                    /** @type {string} Must be true to preview the url specified by href
+                     *
+                    */
+                    'preview',
+                    /** @type {string} Url of service that gets preview.
+                     *
+                     */
+                    'service-url',
+                ];
+            }
+            loadHref() {
+                //const _this = this;
+                if (!this._preview)
+                    return;
+                if (!this._href)
+                    return;
+                if (!this._serviceUrl)
+                    return;
+                fetch(this._serviceUrl + this._href + '&image_no=1&css=true')
+                    .then((response) => {
+                    response.text().then(respText => {
+                        let massagedText = respText;
+                        //console.log(massagedText);
+                        const replacements = [['html', 'div'], ['head', 'header'], ['body', 'main']];
+                        replacements.forEach(s => {
+                            massagedText = massagedText.replace('<' + s[0] + '>', '<' + s[1] + '>').replace('</' + s[0] + '>', '</' + s[1] + '>');
+                        });
+                        massagedText = massagedText.replace('<a href="', '<a target="_blank" href="');
+                        //console.log(massagedText);
+                        massagedText = massagedText.replace('<div id="toolbar" class="clearfix"><button id="changeimg">></button></div>', '');
+                        //const massagedText = respText.replace('<html>', '<div>')
+                        this.shadowRoot.innerHTML = massagedText;
+                    });
+                });
+            }
+            attributeChangedCallback(name, oldValue, newValue) {
+                switch (name) {
+                    case 'href':
+                        this._href = newValue;
+                        // if(this._once) this.loadHref();
+                        this.loadHref();
+                        break;
+                    case 'preview':
+                        this._preview = newValue !== null;
+                        this.loadHref();
+                        break;
+                    case 'service-url':
+                        this._serviceUrl = newValue;
+                        this.loadHref();
+                        break;
+                }
+            }
+        }
+        customElements.define('xtal-link-preview', XtalLinkPreview);
+    }
 })();
-// http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=onsen.io&image_no=1&css=true 
+// http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=onsen.io&image_no=1&css=true
 //# sourceMappingURL=xtal-link-preview.js.map
--- xtal-link-preview.ts
+++ xtal-link-preview.ts
@@ -1,98 +1,126 @@
 (function () {
-    if(customElements.get('xtal-link-preview')) return;
-    /** 
-    * `xtal-link-preview`
-    * Provide preview of URL.
-    * 
-    *
-    * @customElement
-    * @polymer
-    * @demo demo/index.html
-    */
-    class XtalLinkPreview extends HTMLElement{
-        constructor(){
-            super();
-            const template = document.createElement('template');
-            template.innerHTML = `
-              <style>
-                :host {
-                  display: block;
-                }
-              </style>
-              <slot></slot>
-            `;
-            this.attachShadow({mode: 'open'});
-            this.shadowRoot.appendChild(template.content.cloneNode(true));
-            this.style.display="block";
-        }
-        _href: string;
-        _preview = false;
-        set href(val: string){
-            this.setAttribute('href', val);
-            //this._href = val;
-            //this.loadHref();
-        }
-        get href(){
-            return this._href;
-        }
-        set preview(val: boolean){
-            if(val){
-                this.setAttribute('preview', '');
-            }else{
-                this.removeAttribute('preview');
-            }
-        }
-        get preview(){
-            return this._preview;
-        }
-        static get observedAttributes() {
-            return [
-                 /** @type {string} Url to preview
-                  * 
-                 */
-                'href',
-                /** @type {string} Must be true to preview the url specified by href
-                 * 
-                */
-                'preview'
-            ];
-        }
-        loadHref(){
-            //const _this = this;
-            if(!this._preview) return;
-            if(!this._href) return;
-            fetch('https://cors-anywhere.herokuapp.com/http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=' + this._href + '&image_no=1&css=true')
-            .then((response) =>{
-                response.text().then(respText =>{
-                    let massagedText = respText;
-                    //console.log(massagedText);
-                    const replacements = ['html', 'head', 'body']
-                    replacements.forEach(s =>{
-                        massagedText = massagedText.replace('<' + s + '>', '<div>').replace('</' + s + '>', '</div>');
-                    })
-                    massagedText = massagedText.replace('<a href="', '<a target="_blank" href="');
-                    //console.log(massagedText);
-                    massagedText = massagedText.replace('<div id="toolbar" class="clearfix"><button id="changeimg">></button></div>', '');
-                    //const massagedText = respText.replace('<html>', '<div>')
-                    this.shadowRoot.innerHTML = massagedText;
-                })
-            })
-        }
-        attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-            switch (name) {
-                case 'href':
-                    this._href = newValue;
-                    // if(this._once) this.loadHref();
-                    this.loadHref();
-                    break;
-                case 'preview':
-                    this._preview = newValue !== null;
-                    this.loadHref();
-                    break;
+    if (customElements.get('xtal-link-preview')) return;
+    const cs = document.currentScript as HTMLScriptElement;
+    let customStyle = ''
+    if (cs && cs.dataset.css_path) {
 
+    }else{
+        initXtalLinkPreview();
+    }
+    function initXtalLinkPreview() {
+        /** 
+        * `xtal-link-preview`
+        * Provide preview of URL.
+        * 
+        *
+        * @customElement
+        * @polymer
+        * @demo demo/index.html
+        */
+        class XtalLinkPreview extends HTMLElement {
+            constructor() {
+                super();
+                const template = document.createElement('template');
+                template.innerHTML = `
+          <style>
+            :host {
+              display: block;
+            }
+          </style>
+          <slot></slot>
+        `;
+                this.attachShadow({ mode: 'open' });
+                this.shadowRoot.appendChild(template.content.cloneNode(true));
+                this.style.display = "block";
+            }
+            _serviceUrl: string = 'https://cors-anywhere.herokuapp.com/http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url='
+            get serviceUrl() {
+                return this._serviceUrl;
+            }
+            set serviceUrl(val: string) {
+                this.setAttribute('service-url', val);
+            }
+            _href: string;
+            _preview = false;
+            set href(val: string) {
+                this.setAttribute('href', val);
+                //this._href = val;
+                //this.loadHref();
+            }
+            get href() {
+                return this._href;
+            }
+            set preview(val: boolean) {
+                if (val) {
+                    this.setAttribute('preview', '');
+                } else {
+                    this.removeAttribute('preview');
+                }
+            }
+            get preview() {
+                return this._preview;
+            }
+            static get observedAttributes() {
+                return [
+                    /** @type {string} Url to preview
+                     * 
+                    */
+                    'href',
+                    /** @type {string} Must be true to preview the url specified by href
+                     * 
+                    */
+                    'preview',
+                    /** @type {string} Url of service that gets preview.
+                     * 
+                     */
+                    'service-url',
+                ];
+            }
+            loadHref() {
+                //const _this = this;
+                if (!this._preview) return;
+                if (!this._href) return;
+                if (!this._serviceUrl) return;
+                fetch(this._serviceUrl + this._href + '&image_no=1&css=true')
+                    .then((response) => {
+                        response.text().then(respText => {
+                            let massagedText = respText;
+                            //console.log(massagedText);
+                            const replacements = [['html', 'div'], ['head', 'header'], ['body', 'main']];
+                            replacements.forEach(s => {
+                                massagedText = massagedText.replace('<' + s[0] + '>', '<' + s[1] + '>').replace('</' + s[0] + '>', '</' + s[1] + '>');
+                            })
+                            massagedText = massagedText.replace('<a href="', '<a target="_blank" href="');
+                            //console.log(massagedText);
+                            massagedText = massagedText.replace('<div id="toolbar" class="clearfix"><button id="changeimg">></button></div>', '');
+                            //const massagedText = respText.replace('<html>', '<div>')
+                            this.shadowRoot.innerHTML = massagedText;
+                        })
+                    })
+            }
+            attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+                switch (name) {
+                    case 'href':
+                        this._href = newValue;
+                        // if(this._once) this.loadHref();
+                        this.loadHref();
+                        break;
+                    case 'preview':
+                        this._preview = newValue !== null;
+                        this.loadHref();
+                        break;
+                    case 'service-url':
+                        this._serviceUrl = newValue;
+                        this.loadHref();
+                        break;
+
+                }
             }
         }
+
+        customElements.define('xtal-link-preview', XtalLinkPreview);
     }
-    customElements.define('xtal-link-preview', XtalLinkPreview);
+
+    
 })();
 // http://playground.ajaxtown.com/link_preview/class.linkpreview.php?url=onsen.io&image_no=1&css=true
